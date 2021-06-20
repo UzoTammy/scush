039f48a2f1a2cb6641a733be7d0ed4d5281c0b56
Uzo Nwokoro <uzo.nwokoro@gmail.com>
2021-06-20 14:55:14 +0100

    config variables added

--- ozone/settings.py
+++ ozone/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG_VALUE', default=False, cast=bool)
+# DEBUG = config('DEBUG_VALUE', default=False, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ['scush.herokuapp.com', '127.0.0.1', 'localhost']
 
