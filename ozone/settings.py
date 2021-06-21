d0a458d6b009fad65dce9e816c756174e750aa5c
Uzo Nwokoro <uzo.nwokoro@gmail.com>
2021-06-21 23:05:51 +0100

    modified settings.py

--- ozone/settings.py
+++ ozone/settings.py
@@ -162,7 +162,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465
 EMAIL_USE_SSL = True
-EMAIL_SUBJECT_PREFIX = 'Ozone'
+EMAIL_SUBJECT_PREFIX = 'Ozone: '
 
 EMAIL_HOST_USER = config('EMAIL_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_PASS')
