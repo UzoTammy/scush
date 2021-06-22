f1a9c17fa413fdad3af4709e0c7f14a42926cd9e
Uzo Nwokoro <uzo.nwokoro@gmail.com>
2021-06-22 06:30:56 +0100

    changes staticfiles storage

--- ozone/settings.py
+++ ozone/settings.py
@@ -135,8 +135,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFileStorage'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # this location of uploaded files
