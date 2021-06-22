502eafd90616ab8124f454a6d5b91ebef7276ce4
Uzo Nwokoro <uzo.nwokoro@gmail.com>
2021-06-22 06:08:19 +0100

    resolving collectstatic manifestissue

--- ozone/settings.py
+++ ozone/settings.py
@@ -136,7 +136,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # this location of uploaded files
 MEDIA_URL = '/media/'
