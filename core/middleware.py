42d6d92de6155793a21619aeec28a100810bae4d
Uzo Nwokoro <uzo.tammy@gmail.com>
2024-07-18 08:23:32 -0300

    Restored the initiation of custom middleware

--- core/middleware.py
+++ core/middleware.py
@@ -1,14 +1,14 @@
 from typing import Any
 
-# class CustomMiddleware:
-#     def __init__(self, get_response):
-#         self.get_response = get_response
+class CustomMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
     
-#     def __call__(self, request):
+    def __call__(self, request):
         
-#         response = self.get_response(request)
+        response = self.get_response(request)
         
-#         return response
+        return response
     
     # def process_template_response(self, request, response):
     #     if hasattr(response, 'context_data'):
